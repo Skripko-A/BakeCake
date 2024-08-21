87a2f84c34e762b30138191caaaabcff06e5b984
Aleksandr Skripko <skripkoae@gmail.com>
2024-08-21 14:14:45 +0300

    add __str__

--- BakeCake/models.py
+++ BakeCake/models.py
@@ -10,20 +10,27 @@ class Topping(models.Model):
     title = models.CharField(max_length=25, verbose_name='Название')
     description = models.TextField(null=True, blank=True, verbose_name='Описание')
 
+    def __str__(self):
+        return f'{self.title}'
 
 class Berrie(models.Model):
     title = models.CharField(max_length=25, verbose_name='Название')
     description = models.TextField(null=True, blank=True, verbose_name='Описание')
 
+    def __str__(self):
+        return f'{self.title}'
 
 class Decor(models.Model):
     title = models.CharField(max_length=25, verbose_name='Название')
     description = models.TextField(null=True, blank=True, verbose_name='Описание')
 
+    def __str__(self):
+        return f'{self.title}'
+
 
 class Cake(models.Model):
     title = models.CharField(max_length=25, verbose_name='Название')
-    image = models.ImageField(upload_to='media/images/cakes', verbose_name='Картинка')
+    image = models.ImageField(upload_to='images/cakes', verbose_name='Картинка')
     description = models.TextField(null=True, blank=True, verbose_name='Описание')
     levels_number = models.PositiveIntegerField(verbose_name=' Количество уровней (1, 2, 3')
     shape = models.CharField(max_length=25, verbose_name='Форма (круг, квадрат, прямоугольник')
@@ -35,6 +42,9 @@ class Cake(models.Model):
                               related_name='cakes', null=True, blank=True)
     inscription = models.CharField(max_length=25, verbose_name='Надпись', blank=True, null=True)
 
+    def __str__(self):
+        return f'{self.title}'
+
 
 
 class Order(models.Model):
@@ -43,3 +53,6 @@ class Order(models.Model):
     address = models.TextField(verbose_name='Адрес доставки')
     date = models.DateField(verbose_name='Дата', default=(datetime.today() + timedelta(days=1)))
     phone = PhoneNumberField(unique=True)
+
+    def __str__(self):
+        return f'{self.cake}{self.customer}'
