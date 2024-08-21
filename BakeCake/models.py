e0aa335630333d49056ea29738ea9e882d4b4099
Aleksandr Skripko <skripkoae@gmail.com>
2024-08-21 16:16:24 +0300

    add choices for Cake.shape

--- BakeCake/models.py
+++ BakeCake/models.py
@@ -30,15 +30,12 @@ class Decor(models.Model):
 
 
 class Cake(models.Model):
-    class CakeShape(models.TextChoices):
-        round = 'круг'
-        square = 'квадрат'
-        rectangle = 'прямоугольник'
+    shapes = {'ROUND': 'круг', 'SQUARE':'квадрат', 'RECTANGLE': 'прямоугольник'}
     title = models.CharField(max_length=25, verbose_name='Название')
     image = models.ImageField(upload_to='images/cakes', verbose_name='Картинка')
     description = models.TextField(null=True, blank=True, verbose_name='Описание')
     levels_number = models.PositiveIntegerField(verbose_name=' Количество уровней (1, 2, 3')
-    shape = models.CharField(choices=CakeShape.choices, max_length=25, verbose_name='Форма (круг, квадрат, прямоугольник')
+    shape = models.CharField(choices=shapes, max_length=25, verbose_name='Форма (круг, квадрат, прямоугольник')
     topping = models.ForeignKey(Topping, verbose_name='Топпинг', on_delete=models.SET_NULL,
                                 related_name='cakes', null=True, blank=True)
     berrie = models.ForeignKey(Berrie, verbose_name='Ягода', on_delete=models.SET_NULL,
@@ -46,8 +43,6 @@ class Cake(models.Model):
     decor = models.ForeignKey(Decor, verbose_name='Декор', on_delete=models.SET_NULL,
                               related_name='cakes', null=True, blank=True)
     inscription = models.CharField(max_length=25, verbose_name='Надпись', blank=True, null=True)
-
-
 
     def __str__(self):
         return f'{self.title}'
@@ -57,7 +52,7 @@ class Order(models.Model):
     cake = ForeignKey(Cake, verbose_name='Заказ', on_delete=PROTECT, related_name='orders')
     customer = ForeignKey(Client, verbose_name=' Клиент', on_delete=models.CASCADE, related_name='orders')
     address = models.TextField(verbose_name='Адрес доставки')
-    date = models.DateField(verbose_name='Дата', default=(datetime.today() + timedelta(days=1)))
+    date = models.DateField(verbose_name='Дата')
     phone = PhoneNumberField(unique=True)
 
     def __str__(self):
