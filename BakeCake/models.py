42845c4b7cc4b597f9e6e2fa3ea5ca6ced430acc
Aleksandr Skripko <skripkoae@gmail.com>
2024-08-22 13:26:30 +0300

    move Cake.shape and Cake.levels_number to Shape model and Layer model

--- BakeCake/models.py
+++ BakeCake/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-from django.db.models import ForeignKey, SET_NULL, PROTECT
+from django.db.models import ForeignKey, SET_NULL, PROTECT, CharField, DecimalField
 from phonenumber_field.modelfields import PhoneNumberField
 
 from accounts.models import Client
@@ -60,3 +60,13 @@ class Order(models.Model):
 
     def __str__(self):
         return f'{self.cake}{self.customer}'
+
+
+class Shape(models.Model):
+    title = CharField(max_length=25, verbose_name='Форма')
+    price = models.DecimalField(max_digits=10, decimal_places=2, default=100)
+
+
+class Layer(models.Model):
+    models.PositiveIntegerField(verbose_name=' Количество уровней (1, 2, 3')
+    price = models.DecimalField(max_digits=10, decimal_places=2, default=100)
