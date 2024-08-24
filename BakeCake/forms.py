6d5103e4cff420429f5812bf18f702b9294ebee3
lypavel <157053921+lypavel@users.noreply.github.com>
2024-08-24 09:37:36 +0000

    Add time and date validation, and 20% overprice

--- BakeCake/forms.py
+++ BakeCake/forms.py
@@ -1,4 +1,5 @@
 from django import forms
+from django.utils import timezone
 
 from .models import Order
 
@@ -8,3 +9,20 @@ class OrderRegularCakeForm(forms.ModelForm):
         model = Order
         fields = ('name', 'email', 'phone', 'address',
                   'date', 'time', 'comment', 'delivery_comment')
+
+    def clean_date(self):
+        date = self.cleaned_data.get('date')
+        today = timezone.now().date()
+        if date and date < today:
+            raise forms.ValidationError(
+                'Дата не может быть раньше сегодняшней.'
+            )
+        return date
+
+    def clean_time(self):
+        time = self.cleaned_data.get('time')
+        if time and not (10 <= time.hour <= 23):
+            raise forms.ValidationError(
+                'Время должно быть между 10:00 и 23:00'
+            )
+        return time
